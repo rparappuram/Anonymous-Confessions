4b07979942bf1f3293a7d44409ed2410fa6f0d16
RitvikRenikunta <ritvik.renikunta@gmail.com>
2021-10-30 14:13:44 -0500

    Refactored directory

---
+++ website/SpamModel/SpamService.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, jsonify, request
+
+from Model import Model
+
+spam_service = Blueprint('spam_service', __name__)
+
+@spam_service.route('/get_spam', methods=['PUT'])
+def textCompare1():
+    input = request.json
+    text = input['text']
+    SpamModel = Model("spam.csv")
+    output = SpamModel.predict(text)
+    result = {}
+    result['label'] = output[0]
+    return jsonify(result)
